d8e5f5b94dc6f1e2db3e6e36c44f6475cf6b5f47
luo fei <luofei163@126.com>
2015-12-06 15:12:38 +0800

    Initial Commit

---
+++ cplusplus_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus_test
+//
+//  Created by luofei on 12/6/15.
+//  Copyright © 2015 luofei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
